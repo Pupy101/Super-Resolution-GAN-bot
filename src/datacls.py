75ab031c036564912fdab78b7191dece81090bd5
Pupy101 <ser.porkhun41@gmail.com>
2022-03-27 23:29:04 +0300

    Doc-string for dataclasses

--- src/datacls.py
+++ src/datacls.py
@@ -1,13 +1,19 @@
+"""Module with usefull dataclasses."""
+
 from dataclasses import dataclass
 from typing import Any, Dict, List
 
-from torch import nn, optim, Tensor, device
+import torch
+
+from torch import nn, optim
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms as T
 
 
 @dataclass  # data.augmentation
 class Augmentation:
+    """Parameters for train/valid aigmentations."""
+
     mean: List[float]
     std: List[float]
     main: T.Compose
@@ -17,6 +23,8 @@ class Augmentation:
 
 @dataclass
 class InferenceAugmentation:
+    """Parameters for inference preprocessing images."""
+
     mean: List[float]
     std: List[float]
     main: T.Compose
@@ -25,74 +33,110 @@ class InferenceAugmentation:
 
 @dataclass  # train.train
 class GANParameters:
+    """Base class for model parameter."""
+
     generator: Any
     discriminator: Any
 
 
 @dataclass
 class TorchModuleSubclass(GANParameters):
-    def state_dict(self) -> Dict[str, Dict[str, Tensor]]:
+    """Class for save and load combinated model (generator/discriminator)."""
+
+    def state_dict(self) -> Dict[str, Dict[str, torch.Tensor]]:
+        """
+        Save model.
+
+        Returns
+        -------
+        dict with keys 'generator' and 'discriminator'
+        """
         return {
             "generator": self.generator.state_dict(),
             "discriminator": self.discriminator.state_dict(),
         }
 
-    def load_state_dict(self, weight: Dict[str, Dict[str, Tensor]]) -> None:
-        assert "generator" in weight, "Not right weights. Not founded generator"
-        assert "discriminator" in weight, "Not right weights. Not founded discriminator"
+    def load_state_dict(self, weight: Dict[str, Dict[str, torch.Tensor]]) -> None:
+        """
+        Load model.
+
+        Parameters
+        ----------
+        weight : dict with keys 'generator' and 'discriminator'
+        """
+        assert "generator" in weight, "Not founded 'generator' in weights"
+        assert "discriminator" in weight, "Not founded 'discriminator' in weights"
         self.generator.load_state_dict(weight["generator"])
         self.discriminator.load_state_dict(weight["discriminator"])
 
 
 @dataclass
 class Model(TorchModuleSubclass):
+    """Combinated model with generator and discriminator."""
+
     generator: nn.Module
     discriminator: nn.Module
 
 
 @dataclass
 class Optimizer(TorchModuleSubclass):
+    """Optimizers for generator and discriminator."""
+
     generator: optim.Optimizer
     discriminator: optim.Optimizer
 
 
 @dataclass
 class CriterionGenerator:
+    """Criterions for generator with VGGLos + MSELoss and BCELoss."""
+
     mse_vgg: nn.Module
     bce: nn.Module
 
 
 @dataclass
 class Criterion(GANParameters):
+    """Critetions for generator and discriminator."""
+
     generator: CriterionGenerator
     discriminator: nn.Module
 
 
 @dataclass
 class Datasets:
+    """Train and validation datasets."""
+
     train: Dataset
     valid: Dataset
 
 
 @dataclass
-class Dataloaders(Datasets):
+class Dataloaders:
+    """Train and validation dataloaders."""
+
     train: DataLoader
     valid: DataLoader
 
 
 @dataclass
 class CombinedLossOutput:
-    loss1: Tensor
-    loss2: Tensor
+    """Losses values from combinated loss."""
+
+    loss1: torch.Tensor
+    loss2: torch.Tensor
 
 
 @dataclass
 class DiscriminatorLoss:
+    """Loss metric from training discriminator."""
+
     avg: float
 
 
 @dataclass
 class GeneratorLoss:
+    """Loss metric from training generator."""
+
     avg: float
     bce: float
     mse: float
@@ -101,15 +145,19 @@ class GeneratorLoss:
 
 @dataclass
 class MetricResult:
+    """Loss metric from training generator and discriminator."""
+
     discriminator: DiscriminatorLoss
     generator: GeneratorLoss
 
 
 @dataclass  # src.inference.consumer
 class InferenceConfig:
+    """Config for starting inference consumer."""
+
     model: nn.Module
     weight: str
-    device: device
+    device: torch.device
     batch_size: int
     input_dir: str
     target_dir: str
